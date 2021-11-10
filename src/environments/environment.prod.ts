d706ca092724fdc08865c6c642ef2c8188dd126a
oussamafabriquenumerique <jomaa@fabriquenumerique.fr>
2021-11-10 11:05:40 +0100

    first

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'el-camino-6e5d3',
+    appId: '1:437554991774:web:cc1d63ec7815fb79ae218f',
+    storageBucket: 'el-camino-6e5d3.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyD5mZ7Y3zMxrixeiRzE4xg3W7NXvREZUME',
+    authDomain: 'el-camino-6e5d3.firebaseapp.com',
+    messagingSenderId: '437554991774',
+    measurementId: 'G-0DEXSSW9M0',
+  },
+  production: true
+};
